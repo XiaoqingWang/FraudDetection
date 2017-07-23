534397c9efb12112ba3444adcff1f7323beecc45
JeemyJohn <jeemy145@outlook.com>
2017-07-23 14:11:58 +0800

    欺诈检测项目

---
+++ src/main/java/Demo.java
@@ -0,0 +1,18 @@
+import com.alibaba.fastjson.JSON;
+import tools.EsClient;
+
+import java.util.*;
+
+
+/**
+ * Created by Administrator on 2017/6/8.
+ */
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
---
+++ src/main/java/strategies/ruleengine/RuleBasedDetection.java
@@ -0,0 +1,11 @@
+package strategies.ruleengine;
+
+/**
+ * Created by Administrator on 2017/7/11.
+ */
+public class RuleBasedDetection {
+
+    public static void main(String[] args) {
+
+    }
+}
